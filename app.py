c2754b8683f700bf9006a5a46104ba9e166a6688
Patrick Kleindienst <pk070@hdm-stuttgart.de>
2017-05-25 19:19:32 +0200

    Fixed missing import and getenv method typo

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -6,7 +7,7 @@ app = Flask(__name__)
 def home():
     return "Welcome to Software Development for Cloud Computing!"
 
-port = os.getEnv('PORT', 5000)
+port = os.getenv('PORT', 5000)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(port))
